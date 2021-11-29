82701046965a9236a9104eb5c67fdd70b67947f8
Evandroad <evandro.domingues@hotmail.com>
2021-11-29 07:14:23 -0300

    adapter

---
+++ app/src/main/java/com/example/cep/AddressAdapter.java
@@ -0,0 +1,42 @@
+package com.example.cep;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class AddressAdapter extends BaseAdapter {
+
+    private List<Address> addresses;
+    private Activity activity;
+
+    public AddressAdapter(Activity activity, List<Address> addresses) {
+        this.activity = activity;
+        this.addresses = addresses;
+    }
+
+    @Override
+    public int getCount() {
+        return addresses.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return addresses.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        View v = activity.getLayoutInflater().inflate(R.layout.item, viewGroup, false);
+
+        return v;
+    }
+}
