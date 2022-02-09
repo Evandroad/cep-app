699302fcd9381045e0e737db01532fec182f1ae8
Evandro Abreu Domingues <evandro.domingues@hotmail.com>
2022-02-09 15:51:26 -0300

    lib gson local

--- app/src/main/java/com/example/cep/MainActivity.java
+++ app/src/main/java/com/example/cep/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.cep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
