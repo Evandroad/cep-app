1e34782cb95c1c4b8771d728877eaa927a46d627
Evandro Abreu Domingues <50432810+evandroad@users.noreply.github.com>
2022-09-09 10:07:03 -0300

    Update Address.java

--- app/src/main/java/com/example/cep/Address.java
+++ app/src/main/java/com/example/cep/Address.java
@@ -14,93 +14,38 @@ public class Address {
     private String ddd;
     private String siafi;
 
-    public int getStatus() {
-        return status;
-    }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
+    
+    public String getCep() { return cep; }
+    public void setCep(String cep) { this.cep = cep; }
+    
+    public String getLogradouro() { return logradouro; }
+    public void setLogradouro(String logradouro) { this.logradouro = logradouro; }
+    
+    public String getComplemento() { return complemento; }
+    public void setComplemento(String complemento) { this.complemento = complemento; }
+    
+    public String getBairro() {  return bairro; }
+    public void setBairro(String bairro) { this.bairro = bairro; }
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
+    public String getLocalidade() { return localidade; }
+    public void setLocalidade(String localidade) { this.localidade = localidade; }
 
-    public String getCep() {
-        return cep;
-    }
+    public String getUf() { return uf; }
+    public void setUf(String uf) { this.uf = uf; }
 
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
+    public String getIbge() { return ibge; }
+    public void setIbge(String ibge) { this.ibge = ibge; }
 
-    public String getLogradouro() {
-        return logradouro;
-    }
+    public String getGia() { return gia; }
+    public void setGia(String gia) { this.gia = gia; }
 
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
+    public String getDdd() { return ddd; }
+    public void setDdd(String ddd) { this.ddd = ddd; }
 
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getLocalidade() {
-        return localidade;
-    }
-
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public String getIbge() {
-        return ibge;
-    }
-
-    public void setIbge(String ibge) {
-        this.ibge = ibge;
-    }
-
-    public String getGia() {
-        return gia;
-    }
-
-    public void setGia(String gia) {
-        this.gia = gia;
-    }
-
-    public String getDdd() {
-        return ddd;
-    }
-
-    public void setDdd(String ddd) {
-        this.ddd = ddd;
-    }
-
-    public String getSiafi() {
-        return siafi;
-    }
-
-    public void setSiafi(String siafi) {
-        this.siafi = siafi;
-    }
+    public String getSiafi() { return siafi; }
+    public void setSiafi(String siafi) { this.siafi = siafi; }
 
     @Override
     public String toString() {
@@ -112,4 +57,5 @@ public class Address {
                ", uf: " + uf +
                ", ddd: " + ddd;
     }
+    
 }
